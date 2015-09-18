cf10d4dc36e2a0886d0f211b357a06fd809e2e82
unknown <carattino@physics.ledienuniv.nl>
2015-09-18 17:42:06 +0200

    Started to define the MainWindows for the trap. Missing the saving functions. The PowerSpectra is a Work In Progress

--- GUI/Trap/APD.py
+++ GUI/Trap/APD.py
@@ -3,15 +3,117 @@ Created on 18 sep. 2015
 
 @author: carattino
 '''
+import numpy as np
+from pyqtgraph.Qt import QtCore, QtGui
+import sys
+import pyqtgraph as pg
+import _session
+from PyQt4.Qt import QApplication
 
-class MyClass(object):
-    '''
-    classdocs
-    '''
-
-
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+class APD(QtGui.QMainWindow):
+    """ Main window for the APD timetrace. 
+    """
+    def __init__(self,parent=None):
+        super(APD,self).__init__()
         
+        # Layout
+        self.setWindowTitle('APD Timetraces')
+        self.setGeometry(30,30,450,900)
+        self.timetraces = ApdWidget()
+        self.setCentralWidget(self.timetraces)
+        
+        # Initialization of the parameters
+        num_points = int(_session.apdtime/_session.apdacc)
+        x = np.linspace(0, _session.apdtime, num_points)
+        y = np.zeros(num_points)
+        self.curveapd = self.timetraces.apd.plot(x,y,symbol='o',pen=None) 
+        
+        
+        self.workThread = APDThread()
+        self.connect(self.workThread, QtCore.SIGNAL('APD'),self.updateGUI)
+        self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_S), self), QtCore.SIGNAL('activated()'), self.fileSave)
+        self.timetraces.triggerAPD.clicked[bool].connect(self.start_timetrace)
+        
+        exitAction = QtGui.QAction(QtGui.QIcon('GUI/Icons/Signal-stop-icon.png'),'Exit',self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Quit the timetrace monitor in a safe way')
+        exitAction.triggered.connect(self.exitSafe)
+        
+        saveAction = QtGui.QAction(QtGui.QIcon('GUI/Icons/floppy-icon.png'),'Exit',self)
+        saveAction.setShortcut('Ctrl+S')
+        saveAction.setStatusTip('Saves the currently displayed data to a file')
+        saveAction.triggered.connect(self.fileSave)
+        
+        self.statusBar()
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(saveAction)
+        fileMenu.addAction(exitAction)
+        
+    def updateGUI(self,t,c):
+        """ Updates the timetrace with the values of t and c.
+        """
+        time = t*_session.apdacc
+        self.curveapd.setData(time,c)
+            
+    def fileSave(self):
+        """ Saves the data to a file. 
+        """
+        pass
+            
+    def start_timetrace(self):
+        """ Triggers the working thread. 
+        """
+        self.workThread.start()
+    
+    def exitSafe(self,event):
+        """ Exits the application stopping the working Thread. 
+        """
+        if _session.apdtime>5:
+            # To avoid impatience to force the shutdown.
+            print('Waiting for the acquisition to finish.')
+            print('It may take up to %s more seconds'%_session.apdtime)
+        self.workThread.terminate()
+        self.close()
+    
+class ApdWidget(QtGui.QWidget):
+    """ Simple class for showing the timetrace of the APD. 
+    """
+    def __init__(self,parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setWindowTitle('APD Fast Timetrace')              
+        self.layout = QtGui.QGridLayout(self)
+        
+        # APD Timetrace
+        apd = pg.PlotWidget()
+        apd.setLabel('left','Counts',units='#')
+        apd.setLabel('bottom','Time',units='s')
+        self.apd = apd
+        
+        # Run button for APD Fast timetrace
+        self.triggerAPD = QtGui.QPushButton('Run',self)
+        
+        self.layout.addWidget(self.apd,0,0,11,12)
+        self.layout.addWidget(self.triggerAPD,12,11,1,1)
+        
+class APDThread(QtCore.QThread):
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+
+    def __del__(self):
+        self.wait()
+    
+    def run(self):
+        """ Triggers the ADwin for acquiring an APD fast timetrace. 
+            Returns a list of two arrays, with the arrival time and the number of counts.
+        """
+        apd1 = _session.device['apd1']
+        t,c = _session.adw.get_fast_timetrace(apd1, _session.apdtime, _session.apdacc)
+        self.emit(QtCore.SIGNAL('APD'),np.array(t),np.array(c))
+        return
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mon = APD()
+    mon.show()
+    sys.exit(app.exec_())
--- GUI/Trap/Monitor.py
+++ GUI/Trap/Monitor.py
@@ -4,17 +4,10 @@ Created on 18 sep. 2015
 @author: aj carattino
 '''
 import numpy as np
-from pyqtgraph.Qt import QtCore, QtGui
+from pyqtgraph.Qt import QtGui
 import sys
 import pyqtgraph as pg
-import os 
-from datetime import datetime
-import _session
-
-from lib.xml2dict import variables
-from matplotlib.backend_bases import CloseEvent
 from PyQt4.Qt import QApplication
-from GUI import Monitor
 
 class TimeTraces(QtGui.QMainWindow):
     """ Class for showing the variances in the timetraces from the QPD. 
@@ -23,43 +16,24 @@ class TimeTraces(QtGui.QMainWindow):
         super(TimeTraces,self).__init__()
         self.setWindowTitle('Timetraces')
         self.setGeometry(30,30,450,900)
-        self.layout = QtGui.QGridLayout(self)
-         
+        
+        self.timetraces = TimetraceWidget()
+        self.setCentralWidget(self.timetraces)
         # Initial timetrace data
         self.var = np.zeros([4,250])
         x = np.arange(np.size(self.var,axis=1))
-        # Timetraces
-        qpdx = pg.PlotWidget()
-        qpdy = pg.PlotWidget()
-        diff = pg.PlotWidget()
-        apd1 = pg.PlotWidget()
+
          
-        self.varx = qpdx.plot(x,self.var[0,:],pen='y')
-        qpdx.setLabel('left', "QPD X", units='U.A.')
-        qpdx.setLabel('bottom', "time", units='Steps')
-         
-        self.vary = qpdy.plot(x,self.var[1,:],pen='y')
-        qpdy.setLabel('left', "QPD Y", units='U.A.')
-        qpdy.setLabel('bottom', "time", units='Steps')
-         
-        self.vard = diff.plot(x,self.var[2,:],pen='y')
-        diff.setLabel('left', "Diff", units='U.A.')
-        diff.setLabel('bottom', "time", units='Steps')
-        
-        self.vara = apd1.plot(x,self.var[3,:],pen='y')
-        apd1.setLabel('left','APD', units = 'CPS')
-        apd1.setLabel('bottom','time',units='Steps')
-         
-        self.layout.addWidget(qpdx, 0, 0)
-        self.layout.addWidget(qpdy, 0, 1)
-        self.layout.addWidget(diff, 1, 0)
-        self.layout.addWidget(apd1, 1, 1)
+        self.varx = self.timetraces.qpdx.plot(x,self.var[0,:],pen='y')
+        self.vary = self.timetraces.qpdy.plot(x,self.var[1,:],pen='y')
+        self.vard = self.timetraces.diff.plot(x,self.var[2,:],pen='y')
+        self.vara = self.timetraces.apd1.plot(x,self.var[3,:],pen='y')
         
         
         exitAction = QtGui.QAction(QtGui.QIcon('GUI/Icons/Signal-stop-icon.png'),'Exit',self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Quit the timetrace monitor in a safe way')
-        exitAction.triggered.connect(self.closeEvent)
+        exitAction.triggered.connect(self.close)
          
         self.statusBar()
         menubar = self.menuBar()
@@ -80,9 +54,41 @@ class TimeTraces(QtGui.QMainWindow):
         self.vary.setData(x,self.var[1,:])
         self.vard.setData(x,self.var[0,:])
         self.vara.setData(x,self.var[3,:])
+
+class TimetraceWidget(QtGui.QWidget):
+    """ Widget for displaying the Timetraces. 
+    """
+    def __init__(self,parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        
+        
+        # Timetraces
+        qpdx = pg.PlotWidget()
+        qpdy = pg.PlotWidget()
+        diff = pg.PlotWidget()
+        apd1 = pg.PlotWidget()
+        qpdx.setLabel('left', "QPD X", units='U.A.')
+        qpdx.setLabel('bottom', "time", units='Steps')
+        qpdy.setLabel('left', "QPD Y", units='U.A.')
+        qpdy.setLabel('bottom', "time", units='Steps')
+        diff.setLabel('left', "Diff", units='U.A.')
+        diff.setLabel('bottom', "time", units='Steps')        
+        apd1.setLabel('left','APD', units = 'CPS')
+        apd1.setLabel('bottom','time',units='Steps')        
+        self.qpdx = qpdx
+        self.qpdy = qpdy
+        self.diff = diff
+        self.apd1 = apd1
+        
+        # Layout
+        self.layout = QtGui.QGridLayout(self)
+        self.layout.addWidget(self.qpdx, 0, 0)
+        self.layout.addWidget(self.qpdy, 0, 1)
+        self.layout.addWidget(self.diff, 1, 0)
+        self.layout.addWidget(self.apd1, 1, 1)
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mon = Monitor()
+    mon = TimeTraces()
     mon.show()
     sys.exit(app.exec_())
