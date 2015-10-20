df366218c6430d96e42d2057ac294050e68229b3
Aquiles Carattino <carattino@physics.leidenuniv.nl>
2015-10-20 16:41:23 +0200

    Started to work on moving the spectrometer grating

--- special_tasks/spectrometer.py
+++ special_tasks/spectrometer.py
@@ -8,25 +8,25 @@ from numpy import savetxt
 from time import sleep
 
 def abort(savedir,name,data,header):
-    """ In case of aborting saves data variable to file. With the specified header. 
+    """ In case of aborting saves data variable to file. With the specified header.
     The function checks if the file already exists to avoid overwriting to it.
     """
-    
+
     # Checks trailing / in savedir
     if savedir[-1] != '/' or savedir[-1] != '\\':
         savedir += '\\'
-    
+
     # Checks whether the specified name exists in the given directory
     i = 0
     filename = '%s_abort.dat'%(name)
     while os.path.exists(savedir+filename):
         i += 1
         filename = '%s_%s_abort.dat' %(name,i)
-        
+
     savetxt("%s%s_abort.dat" %(savedir,filename), data,fmt='%s', delimiter=",", header=header)
     print("Aborting execution. Data file saved in %s%s_abort.dat"%(savedir,filename))
     sys.exit(0)
-    
+
 def trigger_spectrometer(adw,digin=1,digout=0,digcheck=2):
     """ Function for triggering the spectrometer and waiting until it finishes acquiring.
         Variables:
@@ -38,12 +38,12 @@ def trigger_spectrometer(adw,digin=1,digout=0,digcheck=2):
                 Connect digin to SCAN
                 Connect digout to External Sync
     """
-    
-    adw.set_digout(digout)   # This triggers the spectrometer if it is set to +edge (as it should be!)   
-    sleep(0.5)    
+
+    adw.set_digout(digout)   # This triggers the spectrometer if it is set to +edge (as it should be!)
+    sleep(0.1)
     while adw.get_digin(digin) or adw.get_digin(digcheck):
         sleep(0.1)
         adw.clear_digout(0) # Tries to clear the digout every time until the acquisition of the spectra is done
-    sleep(1) # Gives enough time to the spectrometer to re arm itself.
-    if adw.get_digin(2) == 1:
+    sleep(0.1) # Gives enough time to the spectrometer to re arm itself.
+    if adw.get_digin(digcheck) == 1:
             raise Exception('Not Clearing digout')
