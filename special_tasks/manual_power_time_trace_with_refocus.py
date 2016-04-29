7343067db0295a907dc2d66f28b913a4da592207
Martin Caldarola <caldarola@physics.leidenuniv.nl>
2016-04-29 16:14:16 +0200

    added the model to the adwin class. now it works properly

--- special_tasks/manual_power_time_trace_with_refocus.py
+++ special_tasks/manual_power_time_trace_with_refocus.py
@@ -30,7 +30,7 @@ try:
     time.sleep(0.5)
 
     # set the configuration of power meter.
-    pmeter.attenuator = True
+    pmeter.attenuator = False
     time.sleep(0.5)
     pmeter.wavelength = 759
     print('Wavelength = '+str(pmeter.wavelength)+' nm')
@@ -43,7 +43,6 @@ except:
     self.pmeter = 0
     print('Problem with Power Meter')
 
-    
 # inizialize adwin
 adw = adq(model='goldII')
 xpiezo = device('x piezo')
@@ -53,24 +52,47 @@ counter1 = device('APD 1')
 counter2 = device('APD 2')
 
 # define parameters for the time trace
-timetrace_time = 5 # In seconds
-integration_time = .01 # In seconds
+timetrace_time = 15 # In seconds
+integration_time = .05 # In seconds
 number_elements = int(timetrace_time/integration_time)
 
-N=20
-Pmin_m = 200
-Pmax_m = 5000 # uw
+# parameters for refocus
+devs = [xpiezo,ypiezo,zpiezo]
+center = [56.64,53.73,51.35]
+dims = [0.4,0.4,0.8]
+accuracy = [0.05,0.05,0.1]
+    
+
+N=1# number of measurements to be done
+# min and max power to be measured (make sure it is lower than the threshold)
+
+Pmin = 150
+Pmax = 250 # uw
 
 # inizialize
 pw =np.zeros((1,N)) # power at bfp
 data=np.zeros((2,N))
 edata = np.zeros((2,N))
+xp = np.zeros((1,N)) # position
+yp = np.zeros((1,N)) # position
+zp = np.zeros((1,N)) # position
 
+# for test
+test = False
 
 # for in the measurement
 for n in range(N):
-    print('N measurement =%s. Power to be set= %s uW'%(str(n),str(np.linspace(Pmin_m,Pmax_m,N)[N-n-1])))
+    print('N measurement =%s / %s. Power to be set= %s uW'%(n,N,np.logspace(np.log10(Pmin*9),np.log10(Pmax*9),N)[n]))
     input('&&&&&&&&&&&&&& Press enter to launch the measurement.')
+    # refocus 
+    if test==False:
+        position = adw.focus_full(counter1,devs,center,dims,accuracy).astype('str')
+        xp[0,n]=position[0]
+        yp[0,n]=position[1]
+        zp[0,n]=position[2]
+        time.sleep(0.1)
+        print('Position = %s um'%(position))
+        
     pw_now=(pmeter.data*1000000)
     #pw.append(pw_now)
     pw[0,n]=pw_now/9 # power at bfp
@@ -82,9 +104,9 @@ for n in range(N):
 
     data[:,n]=np.mean(dd,axis=1)/integration_time
     edata[:,n]=np.std(dd,axis=1)/integration_time
-
-    header = "(Row1: TimeTrace1 (counts), Row2: TimeTrace1 (counts)) integration time: %f seconds"%(integration_time)
-    np.savetxt("%s%s" %(savedir,filename), dd,fmt='%s', delimiter=",", header=header)
+    if test==False:
+        header = "(Row1: TimeTrace1 (counts), Row2: TimeTrace1 (counts)) integration time: %f seconds"%(integration_time)
+        np.savetxt("%s%s" %(savedir,filename), dd,fmt='%s', delimiter=",", header=header)
     Beep(440,500)
     Beep(560,500)
 
@@ -92,19 +114,28 @@ for n in range(N):
 pmeter.finalize()   
 
 
-
-header2 = "(Power (uW), Avg counter1 (cps),Avg counter2 (cps), Std counter1 (cps), std counter1 (cps)"
-np.savetxt("%s%s" %(savedir,'M'+t+'_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(data),np.transpose(edata)),axis=1),fmt='%s', delimiter=",", header=header2)    
+if test==False:
+    header2 = "(Power (uW), Avg counter1 (cps),Avg counter2 (cps), Std counter1 (cps), std counter1 (cps)"
+    np.savetxt("%s%s" %(savedir,'M'+t+'_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(data),np.transpose(edata)),axis=1),fmt='%s', delimiter=",", header=header2)    
 
 # plot 
-plt.figure()
-p1=plt.errorbar(pw[0,:],data[0,:],yerr=edata[0,:], fmt='o')
-p2=plt.errorbar(pw[0,:],data[1,:],yerr=edata[1,:], fmt='o')
-plt.setp(p1,color='r',linewidth='2')
-plt.setp(p2,color='b',linewidth='2')
-plt.xlabel('Power [uW]')
-plt.ylabel('Mean counts in %s'%integration_time)
-plt.show()
+# plt.figure()
+# p1=plt.errorbar(pw[0,:],data[0,:],yerr=edata[0,:], fmt='o')
+# p2=plt.errorbar(pw[0,:],data[1,:],yerr=edata[1,:], fmt='o')
+# plt.setp(p1,color='r',linewidth='2')
+# plt.setp(p2,color='b',linewidth='2')
+# plt.xlabel('Power [uW]')
+# plt.ylabel('Mean counts in %s'%integration_time)
+
+# plt.figure()
+# plt.plot(np.array(range(N)),np.squeeze(xp,axis=0),'bo')
+# plt.plot(np.array(range(N)),np.squeeze(yp,axis=0),'ro')
+# plt.plot(np.array(range(N)),np.squeeze(zp,axis=0),'s')
+# plt.xlabel('N')
+# plt.ylabel('Position in uw')
+
+# plt.show()
+
 
 
 print('End')
