e4834566edac785143199f0a4805b9bf244d7871
Martin Caldarola <caldarola@physics.leidenuniv.nl>
2016-05-02 11:11:21 +0200

    now the triggerered program works, the digital trigger needs to be implemented phyisically but the software works

--- special_tasks/manual_power_time_trace_with_refocus_digital_trigger.py
+++ special_tasks/manual_power_time_trace_with_refocus_digital_trigger.py
@@ -48,11 +48,12 @@ adw = adq(model='goldII')
 xpiezo = device('x piezo')
 ypiezo = device('y piezo')
 zpiezo = device('z piezo')
+devs = [xpiezo,ypiezo,zpiezo]
 counter1 = device('APD 1')
 counter2 = device('APD 2')
 
 # define parameters for the time trace
-timetrace_time = 15 # In seconds
+timetrace_time = 1 # In seconds
 integration_time = .05 # In seconds
 number_elements = int(timetrace_time/integration_time)
 
@@ -63,9 +64,10 @@ dims = [0.4,0.4,0.8]
 accuracy = [0.05,0.05,0.1]
     
 
-N=1# number of measurements to be done
-# min and max power to be measured (make sure it is lower than the threshold)
+N=2# number of measurements to be done
 
+# min and max power to use in the sample (make sure it is lower than the threshold)
+# it is transformed to the power to be measured automatically.
 Pmin = 150
 Pmax = 250 # uw
 
@@ -73,6 +75,9 @@ Pmax = 250 # uw
 pw =np.zeros((1,N)) # power at bfp
 data=np.zeros((2,N))
 edata = np.zeros((2,N))
+bkg=np.zeros((2,N))
+ebkg = np.zeros((2,N))
+
 xp = np.zeros((1,N)) # position
 yp = np.zeros((1,N)) # position
 zp = np.zeros((1,N)) # position
@@ -80,35 +85,66 @@ zp = np.zeros((1,N)) # position
 # for test
 test = False
 
-# for in the measurement
-for n in range(N):
-    print('N measurement =%s / %s. Power to be set= %s uW'%(n,N,np.logspace(np.log10(Pmin*9),np.log10(Pmax*9),N)[n]))
-    input('&&&&&&&&&&&&&& Press enter to launch the measurement.')
-    # refocus 
-    if test==False:
-        position = adw.focus_full(counter1,devs,center,dims,accuracy).astype('str')
-        xp[0,n]=position[0]
-        yp[0,n]=position[1]
-        zp[0,n]=position[2]
-        time.sleep(0.1)
-        print('Position = %s um'%(position))
+# while in the measurement
+n=0
+print('First Measurement. Power to be set= %s uW'%(np.logspace(np.log10(Pmin*9),np.log10(Pmax*9),N)[n]))
+while n <N:
+    IN =int(input('0 o 1:' ))
+    # more needs to be done
+    #IN = adw.get_digin(2)
+    if IN == 1:
         
-    pw_now=(pmeter.data*1000000)
-    #pw.append(pw_now)
-    pw[0,n]=pw_now/9 # power at bfp
-    print('Measured Power =%s uW'%(str(pw_now)))
-    filename = 'M'+t+'_TimeTrace_Power='+str(pw_now)+'uW.dat'
-    print('Taking time trace')
-    dd,ii = adw.get_timetrace_static([counter1,counter2],duration=timetrace_time,acc=integration_time)
-    print('Ended time trace')
-
-    data[:,n]=np.mean(dd,axis=1)/integration_time
-    edata[:,n]=np.std(dd,axis=1)/integration_time
-    if test==False:
-        header = "(Row1: TimeTrace1 (counts), Row2: TimeTrace1 (counts)) integration time: %f seconds"%(integration_time)
-        np.savetxt("%s%s" %(savedir,filename), dd,fmt='%s', delimiter=",", header=header)
-    Beep(440,500)
-    Beep(560,500)
+        # refocus 
+        if test==False:
+            print('Refocusing')
+            position = adw.focus_full(counter1,devs,center,dims,accuracy).astype('str')
+            xp[0,n]=position[0]
+            yp[0,n]=position[1]
+            zp[0,n]=position[2]
+            time.sleep(0.1)
+            print('Final Plce position = %s um'%(position))
+        
+        pw_now=(pmeter.data*1000000)
+        pw[0,n]=pw_now/9 # power at bfp
+        print('Measured Power =%s uW'%(str(pw_now)))
+        
+        filename_p = 'Meas_at_'+t+'_TimeTrace_Pcle_Power='+str(pw_now)+'uW.dat'
+        filename_b = 'Meas_at_'+t+'_TimeTrace_Bkg_Power='+str(pw_now)+'uW.dat'
+        # # time trace and save on the particle
+        print('Taking time trace on the particle')
+        dd,ii = adw.get_timetrace_static([counter1,counter2],duration=timetrace_time,acc=integration_time)
+        print('Ended time trace on the particle')
+        # append data pcle
+        data[:,n]=np.mean(dd,axis=1)/integration_time
+        edata[:,n]=np.std(dd,axis=1)/integration_time
+        if test==False: # save timetrace plce
+            header = "(Row1: TimeTrace1 (counts), Row2: TimeTrace1 (counts)) integration time: %f seconds"%(integration_time)
+            np.savetxt("%s%s" %(savedir,filename_p), dd,fmt='%s', delimiter=",", header=header)
+        
+        # # time trace and save BKG
+        print('Taking time trace BKG')
+        if test==False:
+            bkg_position = [xp[0,n]+1,yp[0,n]+1,zp[0,n]]
+            print('BKG position = '+str(bkg_position))
+            adw.go_to_position(devs,bkg_position)
+            time.sleep(0.1)
+        dd,ii = adw.get_timetrace_static([counter1,counter2],duration=timetrace_time,acc=integration_time)
+        print('Ended time trace BKG')
+        # append data bgg
+        bkg[:,n]=np.mean(dd,axis=1)/integration_time
+        ebkg[:,n]=np.std(dd,axis=1)/integration_time
+        if test==False:
+            header = "(Row1: TimeTrace1 (counts), Row2: TimeTrace1 (counts)) integration time: %f seconds"%(integration_time)
+            np.savetxt("%s%s" %(savedir,filename_b), dd,fmt='%s', delimiter=",", header=header)
+        
+        
+        Beep(440,500)
+        Beep(560,500)
+        n=n+1
+        if n < N:
+            print('Measurement number =%s out of %s. Power to be set= %s uW'%(n+1,N,np.logspace(np.log10(Pmin*9),np.log10(Pmax*9),N)[n]))
+            print('Waiting for trigger...')
+        
 
 # finalize power meter
 pmeter.finalize()   
@@ -116,25 +152,27 @@ pmeter.finalize()
 
 if test==False:
     header2 = "(Power (uW), Avg counter1 (cps),Avg counter2 (cps), Std counter1 (cps), std counter1 (cps)"
-    np.savetxt("%s%s" %(savedir,'M'+t+'_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(data),np.transpose(edata)),axis=1),fmt='%s', delimiter=",", header=header2)    
+    np.savetxt("%s%s" %(savedir,'Meas_at_'+t+'_Pcle_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(data),np.transpose(edata)),axis=1),fmt='%s', delimiter=",", header=header2)    
+    np.savetxt("%s%s" %(savedir,'Meas_at_'+t+'_Bkg_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(bkg),np.transpose(ebkg)),axis=1),fmt='%s', delimiter=",", header=header2)    
 
+    
 # plot 
-# plt.figure()
-# p1=plt.errorbar(pw[0,:],data[0,:],yerr=edata[0,:], fmt='o')
-# p2=plt.errorbar(pw[0,:],data[1,:],yerr=edata[1,:], fmt='o')
-# plt.setp(p1,color='r',linewidth='2')
-# plt.setp(p2,color='b',linewidth='2')
-# plt.xlabel('Power [uW]')
-# plt.ylabel('Mean counts in %s'%integration_time)
+plt.figure()
+p1=plt.errorbar(pw[0,:],data[0,:],yerr=edata[0,:], fmt='o')
+p2=plt.errorbar(pw[0,:],data[1,:],yerr=edata[1,:], fmt='o')
+plt.setp(p1,color='r',linewidth='2')
+plt.setp(p2,color='b',linewidth='2')
+plt.xlabel('Power [uW]')
+plt.ylabel('Mean counts in %s'%integration_time)
 
-# plt.figure()
-# plt.plot(np.array(range(N)),np.squeeze(xp,axis=0),'bo')
-# plt.plot(np.array(range(N)),np.squeeze(yp,axis=0),'ro')
-# plt.plot(np.array(range(N)),np.squeeze(zp,axis=0),'s')
-# plt.xlabel('N')
-# plt.ylabel('Position in uw')
+plt.figure()
+plt.plot(np.array(range(N)),np.squeeze(xp,axis=0),'bo')
+plt.plot(np.array(range(N)),np.squeeze(yp,axis=0),'ro')
+plt.plot(np.array(range(N)),np.squeeze(zp,axis=0),'s')
+plt.xlabel('N')
+plt.ylabel('Position in uw')
 
-# plt.show()
+plt.show()
 
 
 
