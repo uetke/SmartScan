7d1cadec3d52f3b3d0447d6bf218b70d1a42841a
Martin Caldarola <caldarola@physics.leidenuniv.nl>
2016-05-10 19:59:28 +0200

    program to measure manual power dependence on top of a NP and also the background is working

--- special_tasks/manual_power_time_trace_with_refocus_digital_trigger.py
+++ special_tasks/manual_power_time_trace_with_refocus_digital_trigger.py
@@ -1,10 +1,11 @@
 # Script for acquiring timetraces vs intensity manually
 #
-######################### M. Caldarola - April 2016
+######################### M. Caldarola - May 2016
 from __future__ import division
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from math import floor
 from lib.adq_mod import *
 from lib.xml2dict import device,variables
 from datetime import datetime
@@ -15,6 +16,34 @@ from lib.logger import get_all_caller,logger
 from devices.powermeter1830c import PowerMeter1830c as pp
 from winsound import Beep
 
+plt.ion() # enables interactive plot to show big numbers.
+
+########################################################## Here is the info to be filled in before starting
+# define parameters for the time trace
+timetrace_time = 10# In seconds
+integration_time = .05 # In seconds
+number_elements = int(timetrace_time/integration_time)
+
+# parameters for refocus
+experiment_label = 'THG_perylene_Tisa759nm'
+center = [34.37,67.00,53.61]
+dims = [0.5,0.5,0.8]
+accuracy = [0.05,0.05,0.1]
+
+N=15             # number of measurements to be done
+# min and max power to use in the sample (make sure it is lower than the threshold)
+# it is transformed to the power to be measured automatically.
+Pmin = 1
+Pmax = 150 # uw at BFP
+pw_to_set= np.logspace(np.log10(Pmin*9),np.log10(Pmax*9),N)
+
+# for testing
+test = False
+###########################################################
+
+# starting
+
+# inizialize
 
 # generate saving path
 savedir = 'D:\\Data\\' + str(datetime.now().date()) + '\\'
@@ -33,7 +62,7 @@ try:
     # set the configuration of power meter.
     pmeter.attenuator = False
     time.sleep(0.5)
-    pmeter.wavelength = 532
+    pmeter.wavelength = 759
     print('Wavelength = '+str(pmeter.wavelength)+' nm')
     time.sleep(0.5)
     print('Units = '+str(pmeter.units))
@@ -53,27 +82,7 @@ devs = [xpiezo,ypiezo,zpiezo]
 counter1 = device('APD 1')
 counter2 = device('APD 2')
 
-# define parameters for the time trace
-timetrace_time = 2# In seconds
-integration_time = .05 # In seconds
-number_elements = int(timetrace_time/integration_time)
-
-# parameters for refocus
-experiment_label = 'Scattering_532nm'
-devs = [xpiezo,ypiezo,zpiezo]
-center = [46.58,49.38,50.40]
-dims = [0.4,0.4,0.8]
-accuracy = [0.05,0.05,0.1]
-    
-
-N=10# number of measurements to be done
-
-# min and max power to use in the sample (make sure it is lower than the threshold)
-# it is transformed to the power to be measured automatically.
-Pmin = 0.01
-Pmax = 0.3 # uw
-
-# inizialize
+# inizialize the vectors needed
 pw =np.zeros((1,N)) # power at bfp
 data=np.zeros((2,N))
 edata = np.zeros((2,N))
@@ -84,16 +93,23 @@ xp = np.zeros((1,N)) # position
 yp = np.zeros((1,N)) # position
 zp = np.zeros((1,N)) # position
 
-# for test
-test = False
-
 # while in the measurement
 n=0
-print('First Measurement. Power to be set= %s uW'%(np.logspace(np.log10(Pmin*9),np.log10(Pmax*9),N)[n]))
+print('First Measurement. Power to be set= %s uW'%(pw_to_set[n]))
 print('Waiting for triggger...')
+
+# shows a plot with the number to be set in big fontsize
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.text(0,0.5,'P =%.2e'%(pw_to_set[n])+' uW',fontsize=55)
+plt.show()
+plt.pause(0.0001)
+print('Waiting for signal...')
+
 while n <N:
 
     if adw.get_digin(0):
+        plt.close()
         print('Measurement started.')
         Beep(440,200)
         # refocus 
@@ -110,8 +126,8 @@ while n <N:
         pw[0,n]=pw_now/9 # power at bfp
         print('Measured Power =%s uW'%(str(pw_now)))
         
-        filename_p = experiment_label +'Meas_at_'+t+'_TimeTrace_Pcle_Power='+str(pw_now)+'uW.dat'
-        filename_b = experiment_label +'Meas_at_'+t+'_TimeTrace_Bkg_Power='+str(pw_now)+'uW.dat'
+        filename_p = experiment_label +'_Meas_at_'+t+'_TimeTrace_Pcle_Power='+str(pw_now)+'uW.dat'
+        filename_b = experiment_label +'_Meas_at_'+t+'_TimeTrace_Bkg_Power='+str(pw_now)+'uW.dat'
         # # time trace and save on the particle
         print('Taking time trace on the particle')
         dd,ii = adw.get_timetrace_static([counter1,counter2],duration=timetrace_time,acc=integration_time)
@@ -139,14 +155,21 @@ while n <N:
             header = "(Row1: TimeTrace1 (counts), Row2: TimeTrace1 (counts)) integration time: %f seconds"%(integration_time)
             np.savetxt("%s%s" %(savedir,filename_b), dd,fmt='%s', delimiter=",", header=header)
 
-        Beep(440,200)
-        Beep(560,200)
-        Beep(440,200)
+        Beep(440,250)
+        Beep(560,250)
+        Beep(440,250)
+        Beep(440,350)
         
         n=n+1
         if n < N:
             print('Measurement number =%s out of %s. Power to be set= %s uW'%(n+1,N,np.logspace(np.log10(Pmin*9),np.log10(Pmax*9),N)[n]))
-            print('Waiting for trigger...')
+            print('Waiting for trigger...') 
+            # show a new plot with the next value to be set
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.text(0,0.5,'P =%.2e'%(pw_to_set[n])+' uW',fontsize=55)
+            plt.show()
+            plt.pause(0.0001)
         
 
 # finalize power meter
@@ -154,9 +177,14 @@ pmeter.finalize()
 
 
 if test==False:
-    header2 = "(Power (uW), Avg counter1 (cps),Avg counter2 (cps), Std counter1 (cps), std counter1 (cps)"
-    np.savetxt("%s%s" %(savedir,experiment_label +'Meas_at_'+t+'_Pcle_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(data),np.transpose(edata)),axis=1),fmt='%s', delimiter=",", header=header2)    
-    np.savetxt("%s%s" %(savedir,experiment_label +'Meas_at_'+t+'_Bkg_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(bkg),np.transpose(ebkg)),axis=1),fmt='%s', delimiter=",", header=header2)    
+    header2 = "Power (uW), Avg counter1 (cps),Avg counter2 (cps), Std counter1 (cps), std counter2 (cps)"
+    np.savetxt("%s%s" %(savedir,experiment_label +'_Meas_at_'+t+'_Pcle_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(data),np.transpose(edata)),axis=1),fmt='%s', delimiter=",", header=header2)    
+    np.savetxt("%s%s" %(savedir,experiment_label +'_Meas_at_'+t+'_Bkg_averaged.dat'),  np.concatenate((np.transpose(pw),np.transpose(bkg),np.transpose(ebkg)),axis=1),fmt='%s', delimiter=",", header=header2)    
+    
+    header3 = "pw (uW), xp (um), yp (um), zp (um),"
+    np.savetxt("%s%s" %(savedir,experiment_label +'_Meas_at_'+t+'_Final_Pcle_position.dat'),  np.concatenate((np.transpose(pw),np.transpose(xp),np.transpose(yp),np.transpose(zp)),axis=1),fmt='%s', delimiter=",", header=header3)    
+    
+    
 
     
 # plot 
@@ -175,10 +203,11 @@ plt.plot(np.array(range(N)),np.squeeze(zp,axis=0),'s')
 plt.xlabel('N')
 plt.ylabel('Position in uw')
 
+
+
+
 plt.show()
-
-
-
+input('Press enter to finish')
 print('End')
 
 
