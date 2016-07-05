f9cbe3f088dda8ba405faf74ec7795d928b98692
Martin Caldarola <caldarola@physics.leidenuniv.nl>
2016-07-05 17:58:01 +0200

    final version for taking a single lifetime measurement at the current positions

--- special_tasks/single_lifetime.py
+++ special_tasks/single_lifetime.py
@@ -8,7 +8,7 @@ import sys
 import os
 import numpy as np
 ## stop all other processes in the adwin before running this. Done in line 28
-experiment_label = 'S1606_21star_Single_Lifetime_bkg'
+experiment_label = 'S1607_02_PMMA_film_with_EuComplex_1mM_770nm_Single_Lifetime'
 
 print('###################################### started lifetime measurement...')
 print('###################################### ')
@@ -32,22 +32,19 @@ filename = experiment_label+'_'+now+'.dat'
 print('Filename to save: '+ filename)
 
 # define parameters for lifetime measurement
-integration_time = 10E-6+10E-6*2*n # secs
+integration_time = 25E-6            # bin time or integration time [sec]
 detection_time=2E-3                 # detection time [sec]
-on_time=1E-3                        # sec
+on_time=2E-3                        # Laser on time [sec]
 duration = detection_time+on_time   # One run experiment time length [sec]
-times=500             # number of times 
+times=1000                           # number of times to repeat the single cycle.
 # show
 print("integration_time= %e sec. duration= %f sec. on_time= %f sec. times= %i."%(integration_time,duration,on_time,times))
-delay=np.floor(integration_time/adw.time_high)
-print('Expected ticks = '+str(int(times*duration / (delay * adw.time_high))))
-
+print('Expected ticks = '+str(int(times*duration / (np.floor(integration_time/adw.time_high) * adw.time_high))))
+# measure
 a1,a2 = adw.lifetime(apd1,apd2,aom,duration=duration,acc=integration_time,on_time=on_time,times=times)
-
 print('obtained size apd1 ' + str(len(a1)))
 print('obtained size apd2 ' + str(len(a2)))
-
-data = np.vstack((a1,a2))
+data = np.vstack((a1,a2)) # stack data obtained to save.
 # save
 header = "(Row1: TimeTrace1 (counts),Row2: TimeTrace2 (counts)) integration_time= %e sec. duration= %f sec. on_time= %f sec. times= %f ."%(integration_time,duration,on_time,times)
 np.savetxt("%s%s" %(savedir,filename), data,fmt='%s', delimiter=",", header=header)
